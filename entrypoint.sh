a15a03e6b340a9e7fd298400779262b3ed72352b
John-Bryant-O <okerecj85@gmail.com>
2026-04-08 14:51:24 +0100

    fix envsubst to only replace specific variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-envsubst < /etc/prometheus/prometheus.yml > /etc/prometheus/prometheus_final.yml
+envsubst '${GRAFANA_CLOUD_TOKEN} ${DEYE_API_KEY}' < /etc/prometheus/prometheus.yml > /etc/prometheus/prometheus_final.yml
 /bin/prometheus \
   --config.file=/etc/prometheus/prometheus_final.yml \
   --storage.tsdb.path=/prometheus
