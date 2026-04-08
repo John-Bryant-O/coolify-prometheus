cb1c49acdf11e89bf17a73f8fd2b7b412c98b180
John-Bryant-O <okerecj85@gmail.com>
2026-04-08 14:45:43 +0100

    use envsubst for automatic environment variable substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sed -i "s|\${GRAFANA_CLOUD_TOKEN}|${GRAFANA_CLOUD_TOKEN}|g" /etc/prometheus/prometheus.yml
-sed -i "s|\${DEYE_API_KEY}|${DEYE_API_KEY}|g" /etc/prometheus/prometheus.yml
+envsubst < /etc/prometheus/prometheus.yml > /etc/prometheus/prometheus_final.yml
 /bin/prometheus \
-  --config.file=/etc/prometheus/prometheus.yml \
+  --config.file=/etc/prometheus/prometheus_final.yml \
   --storage.tsdb.path=/prometheus
